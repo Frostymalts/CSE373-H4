537182ab3f0637f18a79d5759e0badc444300812
Josh Malters <frostymalts@gmail.com>
2015-05-11 20:00:38 -0700

    Provided Hash Code

--- src/shake_n_bacon/StringHasher.java
+++ src/shake_n_bacon/StringHasher.java
@@ -3,19 +3,29 @@ package shake_n_bacon;
 import providedCode.Hasher;
 
 /**
- * @author <name>
- * @UWNetID <uw net id>
- * @studentID <id number>
- * @email <email address>
+ * @author Joshua Malters, Morgan Evans
+ * @UWNetID maltersj, mnevans
+ * @studentID 1336144
+ * @email maltersj@uw.edu>
+ * 
+ * This class provides a method to hash a string to an integer
+ * value.
  */
 public class StringHasher implements Hasher {
 
 	/**
-	 * TODO Replace this comment with your own as appropriate.
+	 * Returns an integer representation of the provided string.
+	 * @param str String object that will be hashed
+	 * @return integer representation of the hased string
 	 */
 	@Override
 	public int hash(String str) {
-		// TODO Auto-generated method stub
-		return 0;
+		int hashVal = 0;
+		int tableSize = 26053; //arbitrary primary number since table size is unavailable. 
+		
+		for (int i = 0; i < str.length(); i++)
+			hashVal = 37 * hashVal + str.charAt(i);
+		
+		return hashVal;
 	}
 }
--- src/writeupExperiment/StringComparatorTest.java
+++ src/writeupExperiment/StringComparatorTest.java
@@ -6,8 +6,8 @@ public class StringComparatorTest {
 
 	public static void main(String[] args) {
 		StringComparator sc = new StringComparator();			
-		String str1 = "cat";
-		String str2 = "cd";
+		String str1 = "cat1";
+		String str2 = "cat";
 		
 		System.out.println("Java compare value = " + str1.compareTo(str2));
 		System.out.println("sc value = " + sc.compare(str1, str2));
